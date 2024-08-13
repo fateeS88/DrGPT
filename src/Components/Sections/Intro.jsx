722b59274d85a8cf399350e458a1472eac26e7ec
fateee <fatimausman8810@yahoo.com>
2024-08-13 09:30:56 +0100

    intro styling

--- src/Components/Sections/Intro.jsx
+++ src/Components/Sections/Intro.jsx
@@ -2,9 +2,9 @@ import { Link } from 'react-router-dom';
 
 export default function Intro () {
     return (
-        <div>
-            <h2> Your reliable AI assistant </h2>
-            <p> Ask, confirm, and care, one query at a time </p>
+        <div className="intro-container">
+            <h2> Your Reliable AI Assistant </h2>
+            <p> Ask, confirm and care, one query at a time </p>
             <Link to="/signup">
             <button>
                 Get Started
