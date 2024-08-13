ce98869862e37fdfa812a6f6ade51c18649c34a7
fateee <fatimausman8810@yahoo.com>
2024-08-13 08:21:22 +0100

    key features

--- src/Components/Sections/Features.jsx
+++ src/Components/Sections/Features.jsx
@@ -1,7 +1,19 @@
+const features = [
+    {title: "Instant Knowledge", description: "Benefit from fast and precise answers to intricate medical questions, enabling more informed decision-making."},
+    {title: "Current Insights", description: "Access tailored suggestions grounded in the latest clinical guidelines and research developments."},
+    {title: "User Friendly", description: "Engage with a user-friendly interface crafted specifically for the needs of busy medical practitioners."}
+]
+
 export default function Features () {
     return (
         <div>
-            
+            <h2> Key Features </h2>
+            {features.map((feature, index) => (
+                <div  key={index}> 
+                    <h3>{feature.title}</h3>
+                    <p>{feature.description}</p>
+                </div>
+            ))}
         </div>
     )
 }
--- src/Components/Sections/Intro.jsx
+++ src/Components/Sections/Intro.jsx
@@ -1,5 +1,4 @@
 import { Link } from 'react-router-dom';
-import Signup from "../Signup"
 
 export default function Intro () {
     return (
