ac09ed0e20ac841d5b670ece7b81cabc2aa3e983
fateee <fatimausman8810@yahoo.com>
2024-08-13 08:51:00 +0100

    footer

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,20 +1,6 @@
-// export default function Header () {
-//     return(
-//         <nav>
-//             <ul>
-//             <li> <a href="/signin">Sign In</a> </li>
-//             <li> <a href="/signup">Sign Up</a> </li>
-//             <li> <a href="/">Home</a> </li>
-//             <li style={{float: "left", borderRight: "none"}}> 
-//             <a href="/"> <img src="" alt=""/> Logo </a> </li>
-//         </ul>
-//       </nav>
-//     )
-// }
-
 import { Link } from 'react-router-dom';
 
-const Header = () => {
+export default function Header () {
   return (
       <nav>
       <ul>
@@ -27,5 +13,3 @@ const Header = () => {
     </nav>
   )
 }
-
-export default Header
--- src/Components/Sections/Footer.jsx
+++ src/Components/Sections/Footer.jsx
@@ -1,8 +1,22 @@
+// import { Link } from 'react-router-dom';
+
 export default function Footer () {
     return (
-        <div>
-            
-        </div>
+         <footer>
+            <h3> About Us </h3>
+            <ul>
+            <li><a href="/about">Our Story</a></li>
+            <li><a href="/">Privacy Policy</a></li>
+            <li><a href="/">Terms of Service</a></li>
+            </ul>
+
+            <h3> Contact </h3>
+            <ul>
+            <li><a href="Email"> askDrGPT@yahoo.com</a></li>
+            <li><a href="Phone Number"> 081305843728</a></li>
+            <li><a href="Address"> nowhere street, Asokoro, Abuja</a></li>
+            </ul>
+         </footer>
     )
 }
     
