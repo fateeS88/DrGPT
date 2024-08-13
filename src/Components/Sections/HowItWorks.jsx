cdac475f9aabf68f03bc09b11f838f35a0f23638
fateee <fatimausman8810@yahoo.com>
2024-08-13 08:33:38 +0100

    how it works

--- src/Components/Sections/HowItWorks.jsx
+++ src/Components/Sections/HowItWorks.jsx
@@ -1,7 +1,20 @@
+const works = [
+    { title: "Seamless Access", description: "Create an account or sign in to unlock all available features and functionalities." },
+    { title: "Engage with the AI", description: "Ask any question, regardless of complexity, to receive comprehensive and insightful responses." },
+    { title: "Improved Accuracy with Knowledge Graph", description: "Kowledge graph base to ensure accurate, fact-based responses and easy updates to information, maintaining current and relevant information." }
+]
+
+
 export default function How () {
     return (
         <div>
-            
+            <h2> How it Works </h2>
+            {works.map((works, index) => (
+                <div  key={index}> 
+                    <h3>{works.title}</h3>
+                    <p>{works.description}</p>
+                </div>
+            ))}
         </div>
     )
 }
